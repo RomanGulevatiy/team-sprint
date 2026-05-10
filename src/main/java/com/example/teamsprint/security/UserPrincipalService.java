a6f0745d2533dc847f5f7d42dd5b184fa8a9704a
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-05-10 15:31:13 +0300

    refactor: rename CustomUserDetailsService to UserPrincipalService for clarity

--- src/main/java/com/example/teamsprint/security/UserPrincipalService.java
+++ src/main/java/com/example/teamsprint/security/UserPrincipalService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+public class UserPrincipalService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
