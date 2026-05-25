7114a4da778648ef6c743641284e6c9d9ac1980e
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-05-25 15:18:07 +0300

    feat: add validation for username in RegisterRequest

--- src/main/java/com/example/teamsprint/dto/RegisterRequest.java
+++ src/main/java/com/example/teamsprint/dto/RegisterRequest.java
@@ -2,6 +2,8 @@ package com.example.teamsprint.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +15,11 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RegisterRequest {
 
+    @Size(min = 3, max = 20, message = "Username must be {min}-{max} characters long")
+    @Pattern(
+            regexp = "^[A-Za-z0-9_]+$",
+            message = "Username must contain only English letters and digits (no spaces or hyphens)"
+    )
     @NotBlank(message = "Username cannot be null")
     private String username;
 
